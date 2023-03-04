d0246177af7e4f715d9ac48ed25e1aa60ce40b6a
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-03-04 15:24:53 +0530

    chore: initialised routes,constants and makeRequest

---
+++ src/pages/ErrorPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function ErrorPage() {
+  const { errorCode } = useParams();
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <p className="text-2xl">Something went wrong!</p>
+      {errorCode && <p className="text-2xl">{`Error code: ${errorCode}`}</p>}
+    </div>
+  );
+}
+
+export default ErrorPage;
---
+++ src/utils/makeRequest/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import BACKEND_URL from '../../constants/apiEndpoints';
+import { ERROR_ROUTE } from '../../constants/routes';
+
+const makeRequest = async (apiEndPoint, navigate, dynamicConfig = {}) => {
+  try {
+    const requestDetails = {
+      baseURL: BACKEND_URL,
+      url: apiEndPoint.url,
+      method: apiEndPoint.method,
+      ...dynamicConfig
+
+    };
+    const { data } = await axios(requestDetails);
+    return data;
+  } catch (e) {
+    const errorStatus = e.response?.status;
+    if (errorStatus) {
+      navigate(`${ERROR_ROUTE}/${errorStatus}`);
+    } else {
+      navigate(ERROR_ROUTE);
+    }
+    return null;
+  }
+};
+
+export default makeRequest;
